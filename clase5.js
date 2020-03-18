44b7389ca43e9a5eaa1860c13c09b20faef8cb41
rodcko2417 <rodcko@live.com>
2020-03-18 17:42:13 -0600

    El alcance de las funciones!

---
+++ clase5.js
@@ -0,0 +1,11 @@
+// Alcance de las funciones
+var nombre = 'Rodcko' // Variable global
+
+function imprimirNombreEnMayusculas(nombre) { // Esta variable, es solo local para la función (n)
+    nombre = nombre.toUpperCase()
+    console.log(n)
+}
+
+imprimirNombreEnMayusculas()
+
+nombre // en este punto ya esta como RODCKO
