5aede2660462a3086925e9001917c6918933cdf5
rodcko2417 <rodcko@live.com>
2020-03-22 16:40:24 -0600

    Condicional múltiple: switch

---
+++ clase16.js
@@ -0,0 +1,28 @@
+// Condicional múltiple: switch
+// Le pregunta al usuario y lo almacena en una variable
+var signo = prompt('¿Cual es tu signo?')
+
+switch (signo) {
+    case 'acuario':
+        console.log('Horoscopo de acuario')
+        break
+    case 'piscis':
+        console.log('Horoscopo de piscis')
+        break
+    case 'cancer':
+    case 'cáncer':
+        console.log('Horoscopo de cancer')
+        break
+    case 'capricornio':
+        console.log('Horoscopo de capricornio')
+        break
+    case 'sagitario':
+        console.log('Horoscopo de sagitario')
+        break
+    default:
+        console.log('No es un signo zodiacal válido')
+        break
+
+}
+
+console.log(signo)
