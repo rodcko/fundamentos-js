0d08dd02feff8b7a6ed79cab6ca4b07740d2b058
rodcko2417 <rodcko@live.com>
2020-03-22 18:51:22 -0600

    Introduccion a arrays

---
+++ clase17.js
@@ -0,0 +1,45 @@
+// Introduccion a arrays
+
+var rodcko = {
+    nombre: 'Rodcko',
+    apellido: 'Haz',
+    altura: 1.80
+}
+
+var pepito = {
+    nombre: 'Pepito',
+    apellido: 'Abzum',
+    altura: 1.65
+}
+
+var josue = {
+    nombre: 'Josue',
+    apellido: 'Barrios',
+    altura: 1.75
+}
+
+var fernanda = {
+    nombre: 'Fernanda',
+    apellido: 'Ruano',
+    altura: 1.62
+}
+
+var personas = [rodcko, pepito, josue, fernanda]
+
+// Ver todo el arreglo
+personas
+
+// Ubicar un elemento del arreglo
+personas[0] // Se desplegaria el primer elemento, según el subindice
+
+// Ver un atributo de un elemento del arreglo
+personas[0].altura
+personas[0]['altura'] // Ambas maneras son equivalentes
+
+
+// Recorrer el array e imprimir la altura de cada elemento
+
+for (var i = 0; i < personas.length; i++) {
+    var persona = personas[i]
+    console.log(`${persona.nombre} mide ${persona.altura} mts`)
+}
